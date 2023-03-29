4e5f7912762175d6e51c01f5d0accc71a4cfb409
willianmenezess <willmenezes@outlook.com.br>
2023-03-28 22:43:51 -0300

    feat: resolvido requisito 4 - testes para o componente notfound.js

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+describe('Testa o componente <NotFound.js />', () => {
+  test('Teste se a página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    // acessar
+    const titleNotFound = screen.getByRole('heading', {
+      name: /page requested not found/i, level: 2,
+    });
+    // aferir
+    expect(titleNotFound).toBeInTheDocument();
+
+    // acessar
+    const imgNotFound = screen.getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+    });
+    // aferir
+    expect(imgNotFound.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
